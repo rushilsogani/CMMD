6b784fd71f79edb71319e5165c8b34dd110e1f01
Rushil Sogani <rushilsogani14@gmail.com>
2025-02-16 17:53:22 -0600

    Made the CMMD score more fluid
    Changed the parameters for calculating the CMMD score. Also changed the formula it uses.

--- clip.py
+++ clip.py
@@ -16,33 +16,36 @@ class CLIP:
     """Extracts CLIP features from text and images and computes MMD"""
     
     tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-large-patch14")
-    text_inputs = tokenizer(["a woman"], padding=True, return_tensors="pt").to(self.device)
+    text_inputs = tokenizer(["a parrot"], padding=True, return_tensors="pt").to(self.device)
     text_features = self.model.get_text_features(**text_inputs)
 
-    # url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pantheon_Rom_1_cropped.jpg/1920px-Pantheon_Rom_1_cropped.jpg"
-    # image = Image.open(requests.get(url, stream=True).raw)
-    image = Image.open("/home/bkhwaja/vscode/ECE50024/images/distorted.png")
-    image2 = Image.open("/home/bkhwaja/vscode/ECE50024/images/clean.png")
-    image_inputs = self.processor(images=image, return_tensors="pt").to(self.device)
-    image_inputs_2 = self.processor(images=image2, return_tensors="pt").to(self.device)
-    image_features_1 = self.model.get_image_features(**image_inputs)
-    image_features_2 = self.model.get_image_features(**image_inputs_2)
+    #url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pantheon_Rom_1_cropped.jpg/1920px-Pantheon_Rom_1_cropped.jpg"
+    #image = Image.open(requests.get(url, stream=True).raw)
 
-    text_features = text_features / text_features.norm(p=2, dim=-1, keepdim=True)
-    image_features_1 = image_features_1 / image_features_1.norm(p=2, dim=-1, keepdim=True)
-    image_features_2 = image_features_2 / image_features_2.norm(p=2, dim=-1, keepdim=True)
+    for i in range(2, 5):
+      image = Image.open("./images/distorted" + str(i) + ".png")
+      image2 = Image.open("./images/cleanParrot.png")
 
-    text_distribution = text_features.cpu().detach().numpy()
-    image_distribution_1 = image_features_1.cpu().detach().numpy()
-    image_distribution_2 = image_features_2.cpu().detach().numpy()
+      image_inputs = self.processor(images=image, return_tensors="pt").to(self.device)
+      image_inputs_2 = self.processor(images=image2, return_tensors="pt").to(self.device)
+      image_features_1 = self.model.get_image_features(**image_inputs)
+      image_features_2 = self.model.get_image_features(**image_inputs_2)
 
-    mmd = MMD(X=image_distribution_1.T, Y=image_distribution_2.T, sigma=1000, scale=100)
-    comparison = mmd.compute_mmd()  # No multiplication with logit_scale
+      # Normalizing the features seems to make the CMMD score very rigid (doesn't move much)
+      # image_features_1 = image_features_1 / image_features_1.norm(p=2, dim=-1, keepdim=True) 
+      # image_features_2 = image_features_2 / image_features_2.norm(p=2, dim=-1, keepdim=True)
+      
+      text_features = text_features / text_features.norm(p=2, dim=-1, keepdim=True)
+      text_distribution = text_features.cpu().detach().numpy()
 
-    print(f"The comparison between the text and image is: {comparison}")
+      image_distribution_1 = image_features_1.cpu().detach().numpy()
+      image_distribution_2 = image_features_2.cpu().detach().numpy()
 
-    return text_distribution, image_distribution_2
-  
+      mmd = MMD(X = image_distribution_1.T, Y = image_distribution_2.T, sigma = 0.5)
+      comparison = mmd.compute_mmd()
+      scale = 100
+      print(f"The comparison between the text and image is: {scale * comparison}")
+
 def main():
   clip = CLIP()
   clip.image_and_text_feature()  
